e47b57ca5bd48b78929dcd6982c74f2762c75a67
Samuel Dennis III <sdennis@avernoon.com>
2023-04-01 21:55:56 -0400

    Add an additional test

--- features/begin.feature
+++ features/begin.feature
@@ -5,3 +5,4 @@ Feature: This is a feature file
         Given I navigate to the url "www.google.com"
         And the title of the page includes the text "Google"
         # Then let me debug
+        And the url of the page includes the text "google.com"
--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -6,6 +6,10 @@ And('the title of the page includes the text {string}') do |string|
     expect(@browser.title.include?(string)).to be(true)
 end
 
+And('the url of the page includes the text {string}') do |string|
+    expect(@browser.url.include?(string)).to be(true)
+end
+
 Then('let me debug') do
     binding.pry
 end
